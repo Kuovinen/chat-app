965f355d13b624eb0a0423a94744f549684e7d72
RomanZujev <roman.zujev@gmail.com>
2022-04-10 20:15:36 +0300

    moved TypingText to its own component

--- src/App backup.js
+++ src/App backup.js
@@ -6,58 +6,51 @@ import MessageInputField from "./components/MessageInputField.js";
 import styles from "./App.css";
 import ava1 from "./avatar.jpeg";
 import ava2 from "./avatar2.jpeg";
-import TypingText from "./components/TypingText.js";
+
 import emoji from "./emoji.svg";
 
 export default function App() {
-  //State controlled elements:
-  let [chat, setChat] = React.useState([]);
-  const [inputTXT, setInputTXT] = React.useState("");
-
-  // chatlog message elements based on the database
-  // array ommiting deleted messages
-  // owner should be chosen by aslias name
-  function updateChatlog(array) {
-    let history = array.map((element) => {
-      if (!element.deleted) {
-        return (
-          <MessageHstr
-            key={element.id}
-            owner={element.owner === "zuro" ? "mine" : "her"}
-            txt={element.txt}
-            id={element.id}
-            hours={element.hours}
-            minutes={element.minutes}
-            date={element.date}
-            month={element.month}
-            year={element.year}
-            edited={element.edited}
-            attachment={element.attachment}
-          />
-        );
-      }
-    });
-    setChat(history);
-  }
-  // call the server and parse the recieved json message data
   function callApi() {
     fetch("http://localhost:8080/")
       .then((res) => res.json())
-      .then((data) => {
-        updateChatlog(data);
-        console.log(data);
-      })
+      .then((data) => console.log(data))
+
       .catch((err) => {
         console.log(`Got error while trying to access server data.`);
         console.log(err);
       });
   }
-  //generate chalog upon app start
-  React.useEffect(callApi, []);
+  callApi();
+  // chatlog message elements based on the database
+  // array ommiting deleted messages
+  // owner should be chosen by aslias name
+  const history = messageData.map((element) => {
+    if (!element.deleted) {
+      return (
+        <MessageHstr
+          key={element.id}
+          owner={element.owner === "zuro" ? "mine" : "her"}
+          txt={element.txt}
+          id={element.id}
+          hours={element.hours}
+          minutes={element.minutes}
+          date={element.date}
+          month={element.month}
+          year={element.year}
+          edited={element.edited}
+          attachment={element.attachment}
+        />
+      );
+    }
+  });
+
+  //State controlled elements:
+  let [chat, setChat] = React.useState(history);
+  const [inputTXT, setInputTXT] = React.useState("");
 
   function sendMessage(event) {
     event.preventDefault();
-    // if there is text in input field (then it's "trufy")
+    // if there is text in inputfield (then it's "trufy")
     // make a message element with the text value
     inputTXT &&
       setChat((previous) => [
@@ -80,26 +73,23 @@ export default function App() {
 
   return (
     <div className="site">
-      {/*side menu*/}
       <div className="menu">
         <div className="menuBtn"></div>
         <img id="ava2" src={ava2} alt="ava"></img>
       </div>
-      {/*entire chat section*/}
+
       <div className="talk">
-        {/*TOP SECTION OF CHAT*/}
         <section className="contact">
           <input className="search" placeholder="search"></input>
           <h1 className="alias">ALIAS</h1>
           <img id="ava" src={ava1} alt="ava"></img>
         </section>
-        {/*CHAT ITSELF*/}
         <div className="dialogue">
           {chat}
           <div className="chatEnd"></div>
           <div className="chatEnd2"></div>
         </div>
-        {/*INPUT SECTION*/}
+
         <section className="input">
           <img className="emoji" src={emoji} alt="emo"></img>
 
@@ -112,7 +102,6 @@ export default function App() {
 
           <div className="send"></div>
         </section>
-        <TypingText />
       </div>
     </div>
   );
