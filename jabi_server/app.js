e66a3940af8ddf1ff457e1d30106094b2a3c5ade
RomanZujev <roman.zujev@gmail.com>
2022-05-29 20:56:58 +0300

    added CLIENTS array for storing ws and uid assignment to WS when they ask for chat update for future chat update broadcasts

--- jabi_server/app.js
+++ jabi_server/app.js
@@ -3,16 +3,26 @@ const wss = new WebSocket.Server({ port: 8080 });
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-
+const CLIENTS = [];
 function getParsedData() {
   const data = fs.readFileSync(path.resolve(__dirname, "./messageData.json"));
   const parsedData = JSON.parse(data);
   return parsedData;
 }
-
+/*
+try {
+  fs.writeFileSync(
+    path.resolve(__dirname, "./messageData.json"),
+    JSON.stringify([])
+  );
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
+*/
 wss.on("connection", function (ws) {
   console.log("new client connected");
-
+  CLIENTS.push(ws);
   ws.on("message", function (message) {
     const formattedMessage = JSON.parse(message.toString());
     //if GETCODE generate and add empty chatlog and send it's code back
@@ -26,7 +36,6 @@ wss.on("connection", function (ws) {
       const parsedData = getParsedData();
       const parsedDataUpdate = [...parsedData, emptyChatLog];
       const dataString = JSON.stringify(parsedDataUpdate);
-      console.log("created", dataString);
       try {
         fs.writeFileSync(
           path.resolve(__dirname, "./messageData.json"),
@@ -43,6 +52,8 @@ wss.on("connection", function (ws) {
       console.log("received: UPDATECHAT", formattedMessage);
       //get data from chatlog file
       const parsedData = getParsedData();
+      //assign the ws it's chatcode for future ref,when broadcasting messages
+      ws.idcode = formattedMessage.payload;
 
       //find the chatlog who's code coincides with payload of front message
       const desiredLog = parsedData.filter(
@@ -51,7 +62,9 @@ wss.on("connection", function (ws) {
       const messages = JSON.stringify(desiredLog[0].messages);
       //send chatlog contents
       ws.send(JSON.stringify({ action: "update", payload: messages }));
-    } else if (formattedMessage.action === "addMessage") {
+    }
+    //ADD NEW MESSAGE
+    else if (formattedMessage.action === "addMessage") {
       console.log("received: ADDMESSAGE", formattedMessage);
       //get data from chatlog file
       const parsedData = getParsedData();
@@ -60,8 +73,6 @@ wss.on("connection", function (ws) {
       const desiredLog = parsedData.filter(
         (element) => element.code === formatedPayload.code
       )[0];
-      console.log(`formated PAYLOAD:`, formatedPayload);
-      console.log("desired LOG:", desiredLog);
       //add message to desiredLogs messages
       const newMessagesArray = [
         ...desiredLog.messages,
@@ -77,7 +88,6 @@ wss.on("connection", function (ws) {
         (element) => element.code !== formatedPayload.code
       );
       const updatedChatlogList = [desiredLogUpdated, ...otherLogs];
-      console.log(updatedChatlogList);
       const finalArray = JSON.stringify(updatedChatlogList);
       try {
         fs.writeFileSync(
@@ -88,34 +98,18 @@ wss.on("connection", function (ws) {
       } catch (err) {
         console.error(err);
       }
+      const formattednewMessagesArray = JSON.stringify(newMessagesArray);
+      //send chatlog contents
+      CLIENTS.map((element) => {
+        if (element.idcode == formatedPayload.code) {
+          element.send(
+            JSON.stringify({
+              action: "update",
+              payload: formattednewMessagesArray,
+            })
+          );
+        }
+      });
     }
   });
 });
-
-/*
-const express = require("express");
-const fs = require("fs");
-
-
-let data = fs.readFileSync(path.resolve(__dirname, "./messageData.json"));
-data = JSON.parse(data);
-
-const app = express();
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.get("/", (req, res) => {
-  res.write(JSON.stringify(data));
-  res.end();
-});
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
-*/
